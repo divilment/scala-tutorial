27026122919ce6a2d7b46f2e67a14ee7e6153257
John Gilbert <github@divilment.net>
2015-03-01 22:32:09 +0000

    Add simple fibonacci number program.

---
+++ src/main/scala/com/divilment/MyModule.scala
@@ -0,0 +1,17 @@
+package com.divilment
+
+object MyModule {
+
+  private def fib(n: Int) : Int = {
+    def fib_rec(n: Int, nm2: Int, nm1: Int): Int =
+      if (n == 0) nm2
+      else fib_rec(n - 1, nm1, nm2 + nm1)
+
+    fib_rec(n, 0, 1)
+  }
+
+  def main(args: Array[String]): Unit = {
+    println(fib(5))
+  }
+
+}
